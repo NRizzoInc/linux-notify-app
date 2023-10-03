f0b45dd8f98da81984a474c651920e29a61d0c75
Nick Rizzo <nick.m.rizzo@medtronic.com>
2023-10-03 18:20:55 -0400

    notification class compiles

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 
-#include <libnotify/notify.h>
-
+#include <notification_manager.h>
 #include <version.h>
+
+using NotificationManager = notification::NotificationManager;
 
 int main() {
     std::cout << "GIT_SHA1=" << GIT_SHA1 << "\n";
     std::cout << "GIT_BRANCH=" << GIT_BRANCH << "\n";
-    notify_init("Sample");
-    NotifyNotification* n = notify_notification_new ("Hello world",
-                                 "some message text... bla bla",
-                                  0);
-    notify_notification_set_timeout(n, 10000); // 10 seconds
 
-    if (!notify_notification_show(n, 0))
-    {
-        std::cerr << "show has failed" << std::endl;
-        notify_uninit();
-        return -1;
-    }
-    notify_uninit();
+    NotificationManager & notif_handle = notification::NotificationManager::getInstance();
+    notif_handle.doWork();
+
     return EXIT_SUCCESS;
 }
