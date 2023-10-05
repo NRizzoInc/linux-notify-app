583caeedd54db696cd0b35e051fb620eeb2b91d1
Nick Rizzo <nick.m.rizzo@medtronic.com>
2023-10-05 14:24:55 -0400

    [feat] automate version and build metadata

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 
 #include <notification_manager.h>
-#include <version.h>
 
 using NotificationManager = notification::NotificationManager;
 
 int main() {
-    std::cout << "GIT_SHA1=" << GIT_SHA1 << "\n";
-    std::cout << "GIT_BRANCH=" << GIT_BRANCH << "\n";
-
     NotificationManager & notif_handle = notification::NotificationManager::getInstance();
     notif_handle.doWork();
 
